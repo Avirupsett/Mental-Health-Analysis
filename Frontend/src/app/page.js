ddea4b0cf4f02dbb789a3cebbc3dbdcdf10b021b
Avirup Sett <72157037+Avirupsett@users.noreply.github.com>
2024-09-18 01:02:52 +0530

    Fix UI

--- Frontend/src/app/page.js
+++ Frontend/src/app/page.js
@@ -24,9 +24,9 @@ export default async function Home() {
 		<section className="min-h-[100vh]">
 			<Navbar />
 			<div className="container mx-auto flex px-0 py-20 md:py-0 md:flex-row flex-col items-center justify-center md:mt-[-85px]">
-				<div className=" md:w-1/2  flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center md:pl-2.5 lg:pl-0">
+				<div className=" md:w-1/2  flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center px-4 md:pl-2.5 lg:pl-0">
 					<span className="text-purple-700 bg-purple-100 flex px-5 py-1.5 sm:py-2.5 pr-6 rounded-full font-bold mb-4 tracking-wide text-sm sm:text-[18px]"><span className="mr-2 mt-0.5 sm:mt-0"><MdAutoAwesome/></span>AI Powered</span>
-					<h1 className="title-font md:text-5xl lg:text-[56px] text-[38px] mb-5 font-bold font-mono tracking-tight text-light-heading !leading-tight">
+					<h1 className="title-font md:text-5xl lg:text-[56px] text-[38px] mb-5 font-bold font-mono tracking-tight text-light-heading leading-[1.3] sm:!leading-tight">
 						<span className="text-purple-600">Your Personalized</span>
 
 						<br className="" />
