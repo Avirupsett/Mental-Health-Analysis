6c817fb0b4424a2c416fc9ac015ed12fef5aad5b
Avirup Sett <72157037+Avirupsett@users.noreply.github.com>
2024-11-05 17:17:19 +0530

    Fix Client rendering

--- Frontend/src/components/Portal/Assigment/AssignmentUI.js
+++ Frontend/src/components/Portal/Assigment/AssignmentUI.js
@@ -1,29 +1,37 @@
+"use client"
 import MCQAssignment from '../../../components/Portal/Assigment/MCQAssignment'
 import WaitingTime from '../../../components/Portal/Assigment/WaitingTime'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import SAQAssignment from '../../../components/Portal/Assigment/SAQAssignment'
-import { cookies } from 'next/headers'
 
 
-export default async function Assignment(props) {
+export default function Assignment() {
+  const [nextAssignmentDate,setNextAssignmentDate] = useState(null);
+  const [latestQAAssignment,setLatestQAAssignment] = useState(null);
+  const [qaAssignmentsCount,setQaAssignmentsCount] = useState(null);
 
   async function GetAssignment(){
-  const cookieStore = cookies();
-   const response = await fetch(`${process.env.URL}/api/assignment`, {
+  
+   const response = await fetch(`/api/assignment`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "Cookie": `wos-session=${cookieStore.get("wos-session")?.value}`
     }
    })
-   return response.json();
+   const data = await response.json();
+   setNextAssignmentDate(data.next_assignment_date);
+   setLatestQAAssignment(data.latestQAAssignment);
+   setQaAssignmentsCount(data.qaAssignmentsCount);
   }
+
+  useEffect(() => {
+    GetAssignment();
+  }, []);
   
-  const {next_assignment_date,latestQAAssignment,qaAssignmentsCount} = await GetAssignment();
   
 
   return (
-    next_assignment_date !== "false" ? <WaitingTime nextAssignmentDate={next_assignment_date} /> : qaAssignmentsCount == null ? <MCQAssignment /> : <SAQAssignment questions={latestQAAssignment.question[0]} />
+    nextAssignmentDate !== "false" ? <WaitingTime nextAssignmentDate={nextAssignmentDate} /> : qaAssignmentsCount == null ? <MCQAssignment /> : <SAQAssignment questions={latestQAAssignment.question[0]} />
 
   )
 }
